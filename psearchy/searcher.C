c5d2991288abcbb51ef6edfdda8fef3e0d90fa8d
Anson <u7139215@anu.edu.au>
2022-01-04 16:41:13 +1100

    fix print

--- psearchy/searcher.C
+++ psearchy/searcher.C
@@ -631,7 +631,7 @@ PostIt* query_term_sst(char *term, int *bufferi, int cid) {
     #endif
 
     if ((w2p_db->get(w2p_db, NULL, &key, &data, 0) != 0) || (data.size != sizeof(offset))) {
-        printf("no such word found in database: %s", w);
+        printf("no such word found in database: %s", term);
         return NULL;
     }
     memcpy(&offset,data.data,sizeof(offset));
