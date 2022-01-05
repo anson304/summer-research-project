de436797dfcafc36b93917cc9b7b335494f06d36
Anson <u7139215@anu.edu.au>
2022-01-05 15:22:56 +1100

    rearrange

--- psearchy/searcher.C
+++ psearchy/searcher.C
@@ -646,8 +646,8 @@ void *doterms(void *arg) {
         }
         //printf("cid: %d, bufferi: %d\n", cid, bufferi);
         // pthread_mutex_lock(&input_lock);
-        printf("Query time: ");
-        print_timer(timer_query, cid);
+        //printf("Query time: ");
+        //print_timer(timer_query, cid);
         reset_Timer(timer_query, cid);
     }
     // pthread_mutex_unlock(&input_lock);
