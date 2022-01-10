0f5bbc1fb8e00518c8642481a60287d46267d316
Anson <u7139215@anu.edu.au>
2022-01-11 02:16:52 +1100

    doterms last

--- psearchy/searcher.C
+++ psearchy/searcher.C
@@ -942,7 +942,7 @@ int main(int argc, char *argv[]) {
     double tailLatSum = 0.0;
 
     for (int r=0; r<REPEATS; r++) {
-        tailLatSum += queryTimeArr[r][max_term-max_term/100]
+        tailLatSum += queryTimeArr[r][max_term-max_term/100];
     }
     printf("tail latency avg: %.6f\n", tailLatSum/REPEATS);
     printf("tail latency last: %.6f\n", queryTimeArr[REPEATS][max_term-max_term/100]);
