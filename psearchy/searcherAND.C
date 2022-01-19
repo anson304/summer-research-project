894a73b2dca42c3908ee5e18c05b23a4fad49542
Anson <u7139215@anu.edu.au>
2022-01-19 15:43:57 +1100

    AND

--- psearchy/searcherAND.C
+++ psearchy/searcherAND.C
@@ -824,6 +824,7 @@ int main(int argc, char *argv[]) {
 
         for (int i = 0; i<2; i++) {
             assert(strlen(terms[max_term][i]) < MAXWORDLENGTH);
+            terms[max_term][i][strlen(terms[max_term][i])-1] = '\n';
             //assert(terms[max_term][i][strlen(terms[max_term][i])-1] == '\n');
             terms[max_term][i][strlen(terms[max_term][i])-1] = '\0';
 
